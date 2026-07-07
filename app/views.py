9d3e9d8c7a802fb1bf33f6c920878f910e4843d5
jmavlonov <jasurmavloniy24@gmail.com>
2026-07-07 19:51:40 +0500

    searching and related products done

--- app/views.py
+++ app/views.py
@@ -2,18 +2,24 @@ from django.shortcuts import render,get_object_or_404,redirect
 from app.models import Category, Product,Comment
 from app.forms import CommentModelForm,OrderModelForm
 from django.contrib import messages
+from django.db.models import Q
 
 
 # Create your views here.
 
 
 def home(request,category_id : int | None = None):
-    
-    categories = Category.objects.all()
+    search_query = request.GET.get("q","")
+    categories = Category.objects.all() # select * from categories;
     products = Product.objects.all()
     
     if category_id is not None:
         products = Product.objects.filter(category = category_id)
+        
+    if search_query is not None:
+        products = Product.objects.filter(Q(name__icontains = search_query) | Q(description__icontains = search_query))
+    
+    
     
     context = {
         'categories':categories,
@@ -27,10 +33,12 @@ def home(request,category_id : int | None = None):
 def product_detail(request,pk):
     product = get_object_or_404(Product,id = pk)
     comments = product.comments.filter(parent__isnull=True).order_by('-created_at')
-
+    related_products = Product.objects.filter(category = product.category).exclude(id=product.id)
+    
     context = {
         'product':product,
-        'comments':comments
+        'comments':comments,
+        'related_products':related_products
     }
     return render(request,'app/detail.html',context)
 
